bb46b6a8fe711ecb1f0f6b9f1dd32af67271fef1
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-09-30 02:08:06 +0000

    automatically backed up by learn

--- db/migrate/20180930015737_create_users.rb
+++ db/migrate/20180930015737_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users
+    create_table :users do |t|
     t.string :username
     t.string :password
     t.float :balance
